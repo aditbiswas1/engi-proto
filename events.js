3dadebfc18c305650ce0d84eecb35973dbb03ac5
Suresh Alse <alseambusher@gmail.com>
2013-06-22 14:20:23 +0530

    sep stuffs

---
+++ events.js
@@ -0,0 +1,39 @@
+publicEvents = [
+	{ name: "tech events", info: "blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	{ name: "mech events", info: "blah-mech-blah"},
+	];
+
+subevents = [
+	{name: "subEvent1", info: "blah blah this is subevent"},
+	{name: "subEvent2", info: "blah blah this is subevent2"},
+	{name: "subEvent3", info: "blah blah this is subevent3"},
+	{name: "subEvent4", info: "blah blah this is subevent4"},
+];
+
+			
+
+
